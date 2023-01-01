d8d487d2212c75457301d92e51f1ce8d4f0b04be
Pablo <percara.pablo@gmail.com>
2023-01-01 15:26:40 -0300

    v.2.0

--- script.js
+++ script.js
@@ -1,8 +1,44 @@
+/***---------------CLASE -------------------------------------- */
+class Producto {
+
+    constructor(codigo,nombre, stock = 0, ubicacion ){
+        this.codigo = codigo;
+        this.nombre = nombre.toUpperCase();
+        this.stock = stock;
+        this.ubicacion = ubicacion.toUpperCase()
+    }
+    //METODOS
+    aumentarStock(cantidad){
+        this.stock += cantidad;
+    }
+    disminuirStock(cantidad){
+        if(this.stock > cantidad){
+            this.stock -= cantidad;
+        }else{alert("No hay suficiente stock disponible")}
+    }
+    cambiarUbicacion(nuevaUbicacion){
+        this.ubicacion = nuevaUbicacion
+    }
+}
+
+/***-------------BASE DE DATOS UTILIZADA -------------------------------- */
+
+let data = [
+    {codigo: '0001', nombre: 'MARTILLO', stock: 20, ubicacion: 'A120302'},
+    {codigo: '0002', nombre: 'CLAVOS', stock: 250, ubicacion: 'A211009'},
+    {codigo: '0003', nombre: 'SIERRA', stock: 20, ubicacion: 'A233009'},
+    {codigo: '0004', nombre: 'PEGAMENTO', stock: 30, ubicacion: 'A010302'},
+    {codigo: '0005', nombre: 'CEMENTO', stock: 50, ubicacion: 'A031009'},
+    {codigo: '0006', nombre: 'CUERDA', stock: 200, ubicacion: 'A232014'}
+]
+
+
+/****------------COMIENZO DE LA FUNCION -------------------------------- */
 
 function inventario (nombreDB){  
   
-    let menu = prompt("Bienvenidos al sistema de gestion de inventario v0.1\n\nSeleccionar una opcion:\n\n1-Ingresar nuevo producto\n2-Consultar Stock disponible\n3-Consultar ubicacion\n4-Dar de baja un producto\n5-Salir")
-    while( menu!=5 ){
+    let menu = prompt("Bienvenidos al sistema de gestion de inventario v0.1\n\nSeleccionar una opcion:\n\n1-Ingresar nuevo producto\n2-Consultar Stock disponible\n3-Consultar ubicacion\n4-Dar de baja un producto\n5-Mostrar base de datos completa\n6-SALIR")
+    while( menu!=6 ){
         //INGRESAR PRODUCTOS
 
         if( menu==1 ){
@@ -31,7 +67,7 @@ function inventario (nombreDB){
                 alert("El producto no fue encontrado!")
             }
 
-            menu = prompt("Bienvenidos al sistema de gestion de inventario v0.1\n\nSeleccionar una opcion:\n\n1-Ingresar nuevo producto\n2-Consultar Stock disponible\n3-Consultar ubicacion\n4-Dar de baja un producto\n5-Salir")
+            menu = prompt("Bienvenidos al sistema de gestion de inventario v0.1\n\nSeleccionar una opcion:\n\n1-Ingresar nuevo producto\n2-Consultar Stock disponible\n3-Consultar ubicacion\n4-Dar de baja un producto\n5-Mostrar base de datos completa\n6-SALIR")
         // CONSULTAR UBICACION
 
         }else if( menu==3){
@@ -43,7 +79,7 @@ function inventario (nombreDB){
             catch(error){
                 alert("El producto no fue encontrado!")
             }
-            menu = prompt("Bienvenidos al sistema de gestion de inventario v0.1\n\nSeleccionar una opcion:\n\n1-Ingresar nuevo producto\n2-Consultar Stock disponible\n3-Consultar ubicacion\n4-Dar de baja un producto\n5-Salir")
+            menu = prompt("Bienvenidos al sistema de gestion de inventario v0.1\n\nSeleccionar una opcion:\n\n1-Ingresar nuevo producto\n2-Consultar Stock disponible\n3-Consultar ubicacion\n4-Dar de baja un producto\n5-Mostrar base de datos completa\n6-SALIR")
         //ELIMINAR UN PRODUCTO
 
         }else if( menu==4){
@@ -60,19 +96,42 @@ function inventario (nombreDB){
                 catch(error){
                     alert("El producto no fue encontrado!")
                 }
-            menu = prompt("Bienvenidos al sistema de gestion de inventario v0.1\n\nSeleccionar una opcion:\n\n1-Ingresar nuevo producto\n2-Consultar Stock disponible\n3-Consultar ubicacion\n4-Dar de baja un producto\n5-Salir")
+                menu = prompt("Bienvenidos al sistema de gestion de inventario v0.1\n\nSeleccionar una opcion:\n\n1-Ingresar nuevo producto\n2-Consultar Stock disponible\n3-Consultar ubicacion\n4-Dar de baja un producto\n5-Mostrar base de datos completa\n6-SALIR")
+        }else if( menu==5){
+            let vista = 'Productos disponibles en la base de datos: \n\n';
+            nombreDB.forEach(item => {vista += item['codigo'] +' '+ item['nombre'] + '\n'});
+            alert(vista);
+            menu = prompt("Bienvenidos al sistema de gestion de inventario v0.1\n\nSeleccionar una opcion:\n\n1-Ingresar nuevo producto\n2-Consultar Stock disponible\n3-Consultar ubicacion\n4-Dar de baja un producto\n5-Mostrar base de datos completa\n6-SALIR")
 
-        }else{
-            alert("La opcion ingresada es incorrecta")
-            menu = prompt("Bienvenidos al sistema de gestion de inventario v0.1\n\nSeleccionar una opcion:\n\n1-Ingresar nuevo producto\n2-Consultar Stock disponible\n3-Consultar ubicacion\n4-Dar de baja un producto\n5-Salir")
         }
-        console.log(nombreDB)
+        else{
+            alert("La opcion ingresada es incorrecta")
+            menu = prompt("Bienvenidos al sistema de gestion de inventario v0.1\n\nSeleccionar una opcion:\n\n1-Ingresar nuevo producto\n2-Consultar Stock disponible\n3-Consultar ubicacion\n4-Dar de baja un producto\n5-Mostrar base de datos completa\n6-SALIR")
+        }
+        
         
     }
-    console.log(nombreDB)
+    
 }
 
+/***----------------LLAMADA A LA FUNCION -------------------------------------------- */
 
+
+/*function mostrarInventario(nombreDB){
+    let vista = 'Productos disponibles en la base de datos: \n\n';
+    nombreDB.forEach(item => {vista += item['codigo'] +' '+ item['nombre'] + '\n';
+    return vista;
+    });
+    alert(vista);
+    
+
+}*/
+
+
+
+inventario(data)
+
+//mostrarInventario(data)
 
 
 
